45a5fb0cf7485356bd5abc65caea4b4a3e9cb4b3
Peter Amstutz <peter.amstutz@curoverse.com>
2015-04-24 17:10:39 -0400

    More command line rewriting.  Fix incorrect use of regex matching.  Cleaned up commented out code.

--- public/gatk-queue/src/main/scala/org/broadinstitute/gatk/queue/engine/arvados/ArvadosJobRunner.scala
+++ public/gatk-queue/src/main/scala/org/broadinstitute/gatk/queue/engine/arvados/ArvadosJobRunner.scala
@@ -46,6 +46,8 @@ class ArvadosJobRunner(val arv: Arvados, val function: CommandLineFunction) exte
   /** Job Id of the currently executing job. */
   var jobUuid: String = _
   override def jobIdString = jobUuid
+  var outfilePath: String = ""
+  var outfileName: String = ""
 
   // Set the display name to < 512 characters of the description
   // NOTE: Not sure if this is configuration specific?
@@ -55,111 +57,91 @@ class ArvadosJobRunner(val arv: Arvados, val function: CommandLineFunction) exte
 
   def start() {
     arv.synchronized {
-    var body = new HashMap[String, Object]()
-    body.put("script", "run-command")
-    body.put("script_version", "master")
-    body.put("repository", "arvados")
+      var body = new HashMap[String, Object]()
+      body.put("script", "run-command")
+      body.put("script_version", "master")
+      body.put("repository", "arvados")
 
-    var cl = function.commandLine
-    var rx = """(-Djava.io.tmpdir=)(/tmp/crunch-job-task-work/compute\d+\.\d+/output/.queue/tmp)""".r
-    cl = rx.replaceAllIn(cl, "$1\\$(task.tmpdir)")
+      var runtime = new HashMap[String, Object]()
+      runtime.put("docker_image", "arvados/jobs-java-bwa-samtools")
+      runtime.put("max_tasks_per_node", 1:java.lang.Integer)
+      body.put("runtime_constraints", runtime)
 
-    rx = """'-o' '(/tmp/crunch-job-task-work/compute\d+\.\d+/output/.queue/scatterGather/[^/]+/[^/]+/)(.*)'""".r
-    cl = rx.replaceAllIn(cl, "'-o' '\\$(task.outdir)/$2'")
+      var cl = function.commandLine
 
-    rx = """'-L' '(/tmp/crunch-job-task-work/compute\d+\.\d+/output/.queue/scatterGather/[^/]+/[^/]+/)(.*)'""".r
+      // Adjust tmpdir
+      var rx = """'-Djava.io.tmpdir=([^']+)'""".r
+      cl = rx.replaceFirstIn(cl, "'-Djava.io.tmpdir=\\$(task.tmpdir)'")
 
-    cl match { case rx(scatterdir, b) => {
-       // Need to shell out to arv-put to upload scatterdir
+      // Adjust thread count
+      rx = """'-nct' '(\d+)'""".r
+      cl = rx.replaceFirstIn(cl, "'-nct' '\\$(node.cores)'")
 
-    val commandLine = Array("arv-put", "--no-progress", "--portable-data-hash", scatterdir)
-    val stdoutSettings = new OutputStreamSettings
-    val stderrSettings = new OutputStreamSettings
-
-    var temp = java.io.File.createTempFile("PDH", ".tmp");
-
-    stdoutSettings.setOutputFile(temp, true)
-
-    val processSettings = new ProcessSettings(
-      commandLine, false, function.commandDirectory, null,
-      null, stdoutSettings, stderrSettings)
-
-    var controller = ProcessController.getThreadLocal
-    val exitStatus = controller.exec(processSettings).getExitValue
-
-    var pdh = Files.readAllLines(Paths.get(temp.getPath()), Charset.defaultCharset()).get(0)
-    println(pdh)
-
+      // Capture and adjust output path
+      rx = """'-o' '([^']+/\.queue/scatterGather/[^/]+/[^/]+/([^']+))'""".r
+      rx.findFirstMatchIn(cl) match {
+        case Some(m) => {
+          outfilePath = m.group(1)
+          outfileName = m.group(1)
+        }
+        case None => {}
       }
-    }
+      cl = rx.replaceFirstIn(cl, "'-o' '$2'")
 
-    cl = rx.replaceAllIn(cl, "'-L' '/keep/" + "abc+123" + "/$2'")
+      // Capture and adjust scatter intervals
+      rx = """'-L' '([^']+/\.queue/scatterGather/[^/]+/[^/]+/)([^']+)'""".r
 
-    var parameters = new HashMap[String, Object]()
-    var cmdLine = new ArrayList[String]
-    cmdLine.add("/bin/sh")
-    cmdLine.add("-c")
-    cmdLine.add(function.commandLine)
-    parameters.put("command", cmdLine)
-    //parameters.put("task.stdout", "output")
+      var vwdpdh = ""
+      rx.findFirstMatchIn(cl) match {
+        case Some(m) => {
+          // Need to shell out to arv-put to upload scatterdir
+          val commandLine = Array("arv-put", "--no-progress", "--portable-data-hash", m.group(1))
+          val stdoutSettings = new OutputStreamSettings
+          val stderrSettings = new OutputStreamSettings
 
-    body.put("script_parameters", parameters)
+          var temp = java.io.File.createTempFile("PDH", ".tmp");
 
-    var json = new JSONObject(body)
-    var p = new HashMap[String, Object]()
-    p.put("job", json.toString())
-    var response = arv.call("jobs", "create", p)
+          stdoutSettings.setOutputFile(temp, true)
 
-    jobUuid = response.get("uuid").asInstanceOf[String]
-    println(jobUuid)
+          val processSettings = new ProcessSettings(
+            commandLine, false, function.commandDirectory, null,
+            null, stdoutSettings, stderrSettings)
 
-    updateStatus(RunnerStatus.RUNNING)
-    }
+          var controller = ProcessController.getThreadLocal
+          val exitStatus = controller.exec(processSettings).getExitValue
+          if (exitStatus != 0) {
+            updateStatus(RunnerStatus.FAILED)
+            return
+          }
 
-  /*
-      // Set the current working directory
-      drmaaJob.setWorkingDirectory(function.commandDirectory.getPath)
-
-      // Set the output file for stdout
-      drmaaJob.setOutputPath(":" + function.jobOutputFile.getPath)
-
-      // If the error file is set specify the separate output for stderr
-      // Otherwise join with stdout
-      if (function.jobErrorFile != null) {
-        drmaaJob.setErrorPath(":" + function.jobErrorFile.getPath)
-      } else {
-        drmaaJob.setJoinFiles(true)
+          vwdpdh = Files.readAllLines(Paths.get(temp.getPath()), Charset.defaultCharset()).get(0)
+          println(vwdpdh)
+        }
+        case None => {}
       }
 
-      if(!function.wallTime.isEmpty)
-    	  drmaaJob.setHardWallclockTimeLimit(function.wallTime.get)
+      cl = rx.replaceFirstIn(cl, "'-L' '$2'")
 
-      drmaaJob.setNativeSpecification(functionNativeSpec)
+      var parameters = new HashMap[String, Object]()
+      var cmdLine = new ArrayList[String]
+      cmdLine.add("/bin/sh")
+      cmdLine.add("-c")
+      cmdLine.add(function.commandLine)
+      parameters.put("command", cmdLine)
+      parameters.put("task.vwd", vwdpdh)
 
-      // Instead of running the function.commandLine, run "sh <jobScript>"
-      drmaaJob.setRemoteCommand("sh")
-      drmaaJob.setArgs(Collections.singletonList(jobScript.toString))
+      body.put("script_parameters", parameters)
 
-      // Allow advanced users to update the request via QFunction.updateJobRun()
-      updateJobRun(drmaaJob)
+      var json = new JSONObject(body)
+      var p = new HashMap[String, Object]()
+      p.put("job", json.toString())
+      var response = arv.call("jobs", "create", p)
+
+      jobUuid = response.get("uuid").asInstanceOf[String]
+      println(jobUuid)
 
       updateStatus(RunnerStatus.RUNNING)
-
-      // Start the job and store the id so it can be killed in tryStop
-      try {
-        Retry.attempt(() => {
-          try {
-            jobId = session.runJob(drmaaJob)
-          } catch {
-            case de: DrmaaException => throw new QException("Unable to submit job: " + de.getLocalizedMessage)
-          }
-        }, 1, 5, 10)
-      } finally {
-        // Prevent memory leaks
-        session.deleteJobTemplate(drmaaJob)
-      }
-      logger.info("Submitted job id: " + jobId)
-      */
+    }
   }
 
   def updateJobStatus() = {
@@ -172,80 +154,28 @@ class ArvadosJobRunner(val arv: Arvados, val function: CommandLineFunction) exte
       var state = response.get("state")
       println(state)
       state match {
-            case "Queued" => returnStatus = RunnerStatus.RUNNING
-            case "Running" => returnStatus = RunnerStatus.RUNNING
-            case "Complete" => {
-              //Files.createSymbolicLink(Paths.get(function.jobOutputFile.getPath),
-              //  Paths.get("/keep/" + response.get("output") + "/output"))
-
-              returnStatus = RunnerStatus.DONE
-            }
-            case "Failed" => returnStatus = RunnerStatus.FAILED
-            case "Cancelled" => returnStatus = RunnerStatus.FAILED
+        case "Queued" => returnStatus = RunnerStatus.RUNNING
+        case "Running" => returnStatus = RunnerStatus.RUNNING
+        case "Complete" => {
+          Files.createSymbolicLink(Paths.get(outfilePath), Paths.get("/keep/" + response.get("output") + "/" + outfileName))
+          returnStatus = RunnerStatus.DONE
+        }
+        case "Failed" => returnStatus = RunnerStatus.FAILED
+        case "Cancelled" => returnStatus = RunnerStatus.FAILED
       }
 
       println(returnStatus)
       updateStatus(returnStatus)
       true
     }
-  /*
-    session.synchronized {
-      var returnStatus: RunnerStatus.Value = null
-
-      try {
-        val jobStatus = session.getJobProgramStatus(jobId);
-        jobStatus match {
-          case Session.QUEUED_ACTIVE => returnStatus = RunnerStatus.RUNNING
-          case Session.DONE =>
-            val jobInfo: JobInfo = session.wait(jobId, Session.TIMEOUT_NO_WAIT)
-            // Update jobInfo
-            def convertDRMAATime(key: String): Date = {
-              val v = jobInfo.getResourceUsage.get(key)
-              if ( v != null ) new Date(v.toString.toDouble.toLong * 1000) else null;
-            }
-            if ( jobInfo.getResourceUsage != null ) {
-              getRunInfo.startTime = convertDRMAATime("start_time")
-              getRunInfo.doneTime = convertDRMAATime("end_time")
-              getRunInfo.exechosts = "unknown"
-            }
-
-            if ((jobInfo.hasExited && jobInfo.getExitStatus != 0)
-                || jobInfo.hasSignaled
-                || jobInfo.wasAborted)
-              returnStatus = RunnerStatus.FAILED
-            else
-              returnStatus = RunnerStatus.DONE
-          case Session.FAILED => returnStatus = RunnerStatus.FAILED
-          case Session.UNDETERMINED => logger.warn("Unable to determine status of job id " + jobId)
-          case _ => returnStatus = RunnerStatus.RUNNING
-        }
-      } catch {
-        // getJobProgramStatus will throw an exception once wait has run, as the
-        // job will be reaped.  If the status is currently DONE or FAILED, return
-        // the status.
-        case de: DrmaaException =>
-          if (lastStatus == RunnerStatus.DONE || lastStatus == RunnerStatus.FAILED)
-            returnStatus = lastStatus
-          else
-            logger.warn("Unable to determine status of job id " + jobId, de)
-      }
-
-      if (returnStatus != null) {
-        updateStatus(returnStatus)
-        true
-      } else {
-        false
-      }
-    }
-    */
   }
 
   def tryStop() {
     try {
-    var p = new HashMap[String, Object]()
-    p.put("uuid", jobUuid)
-    p.put("job", "")
-    var response = arv.call("jobs", "cancel", p)
+      var p = new HashMap[String, Object]()
+      p.put("uuid", jobUuid)
+      p.put("job", "")
+      var response = arv.call("jobs", "cancel", p)
     } catch {
       case e: com.google.api.client.http.HttpResponseException => {}
     }
